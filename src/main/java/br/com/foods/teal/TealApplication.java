7477df1345e1e1945cd913e0650b32fbf0d67758
Caio Pereira <caio.moreira@partners.mv.com.br>
2025-03-12 20:26:34 -0300

    Estrutura do Projeto Criada

---
+++ src/main/java/br/com/foods/teal/TealApplication.java
@@ -0,0 +1,13 @@
+package br.com.foods.teal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/foods/teal/TealApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.foods.teal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
